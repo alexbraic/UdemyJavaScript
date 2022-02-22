1ec1018f756c26a75d23bc946ef087e51fc6d125
Alex <alexandru_braic@yahoo.com>
2022-02-22 21:21:05 +0000

    started arrays

--- script.js
+++ script.js
@@ -215,4 +215,44 @@
 //     }
 // console.log(yearsUntilRetire(1950, 'Alex'));
 
+// // Arrays
 
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const y = new Array(1991, 1984, 2008, 2020);
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// //get array length
+// console.log(friends.length);
+// // get the last element of the array
+// console.log(friends[friends.length - 1]);
+
+// //changing an element
+// friends [2] = 'Jay';
+// console.log(friends);
+
+// //arrays can hold any type of value/expression
+// const firstName = 'Alex';
+// const alex = [firstName, 'Br', 2037 - 1986, 'learner', friends];
+
+// console.log(alex);
+
+// //Exercise
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[2]);
+// const age4 = calcAge(years[years.length - 1]);
+
+// console.log(age1, age2, age3, age4);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2]), calcAge(years[years.length - 1])];
+
+// console.log(ages);
